a08df72d06daf658cd37cb6bedb24688052cc8bf
kumarkapadam <92418147+kumarkapadam@users.noreply.github.com>
2021-12-27 16:46:39 +0530

    27-12-2021

---
+++ practice list/_11_common_elem.py
@@ -0,0 +1,33 @@
+#  common elements from list2
+
+print("*********************   0.Mathematics          *********************")
+
+print("*********************   1. Builtin Functions   *********************")
+
+list1 = [12,19,13,1,5,10,25]
+list2 = [10,12,15,14,13,15,19]
+
+print("************** method-1 using intersection ***********")
+print("common elements in list2")
+x = set(list2) - set(list1)
+print("common elements from list2 ",x)
+
+print("************** method-2 using & symbol ***********")
+print("common elements from list2 ", set(list1) & set(list2))
+
+
+
+
+print(" ********************   2. Algorithm            ********************")
+
+print(" ********************   3 Using Functions       ********************")
+
+print(" ********************   4 Object Oriented       ********************")
+
+print(" ********************   5 Exception handling   ********************")
+
+print(" ********************   6 File Handling         ********************")
+
+print(" ********************   7 Database interaction  ********************")
+
+print(" ********************   8 UI Interaction       ********************")
---
+++ practice list/_19_diff_two_list.py
@@ -0,0 +1,38 @@
+#
+
+print("*********************   0.Mathematics          *********************")
+
+print("*********************   1. Builtin Functions   *********************")
+
+list = [1, 2, 3, 4, 5]
+list1 = [1, 10, 13, 15, 16]
+print("------convert list into set--------")
+list = set(list)
+list1 = set(list1)
+print("difference between two list:", list - list1)
+
+print(" ********************   2. Algorithm            ********************")
+
+list2 = [1, 2, 3, 4, 5]
+list1 = [1, 10, 13, 15, 16]
+if True:
+    list2 = set(list2)
+    list1 = set(list1)
+    list3 = list2-list1
+    list4 = list1-list2
+    print(list3)
+    print(list4)
+else:
+     pass
+
+print(" ********************   3 Using Functions       ********************")
+
+print(" ********************   4 Object Oriented       ********************")
+
+print(" ********************   5 Exception handling   ********************")
+
+print(" ********************   6 File Handling         ********************")
+
+print(" ********************   7 Database interaction  ********************")
+
+print(" ********************   8 UI Interaction       ********************")
